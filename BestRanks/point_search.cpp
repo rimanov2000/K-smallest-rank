082e36a03dfe5ba8f0e1c5277f33496f1df4035e
irufat <44818074+irufat@users.noreply.github.com>
2020-02-04 21:27:53 -0500

    Add project files.

---
+++ BestRanks/point_search.cpp
@@ -0,0 +1,172 @@
+#include "pch.h"
+#include <stdint.h>
+#include <iostream> // For testing
+/* include vector lib for the use of vectors*/
+#include <vector>  
+/*include the header file for function and structure declerations*/
+#include "point_search.h"
+
+/* For min heap algorihtm, I have used https://johnderinger.wordpress.com/2012/12/28/heapify/ and https://www.geeksforgeeks.org/binary-heap/ as a reference */
+	
+
+/*Store all the values unsorted into the vector in O(n), where n is total number of elements in the input*/
+__declspec(dllexport) SearchContext*  create(const Point* points_begin, const Point* points_end){
+		SearchContext* temp = new SearchContext;
+		temp->count = 1;
+		auto it = points_begin; //create a temp variable for going through the container(as an iterator)
+		while (it != points_end) {
+			(temp->count)++;
+			++it; //iterate the counter 
+		}
+		temp->arr = (Point*)malloc(sizeof(Point)*(temp->count));
+		it = points_begin;
+		int count = 0;
+		while (it != points_end) {
+			(temp->arr)[count] = *it;
+			++it; //iterate the pointer 
+			++count; //iterate the counter
+		}
+		return temp;
+	}
+	Point* Minim_Heap::extract_min() {
+		if (heap.size() > 0) {
+			Point* extracted = new Point;
+			*extracted = heap[0];
+			heap[0] = heap[heap.size() - 1];
+			heap.pop_back();
+			min_heapify(0, heap.size() - 1);
+			return extracted;
+		}
+		else
+			return NULL;
+	}
+	void Minim_Heap::create() {
+		// last non-leaf node index
+		int end = heap.size() - 1;
+		int index = (end / 2) - 1;
+
+		// heapify from the first non-leaf node to the root to create the heap
+		for (int i = index; i >= 0; --i) {
+			min_heapify(i, end);
+		}
+	}
+	void Minim_Heap::insert(Point temp) { heap.push_back(temp);  }
+	void Minim_Heap::swap(Point* X, Point* Y) {
+		Point temp = *X;
+		*Y = *X;
+		*X = temp;
+	}
+	void Minim_Heap::min_heapify(int id, int end) {
+		int beg = id;
+		int child_id = 2 * beg + 1;
+		while (child_id <= end) {
+			if (child_id + 1 <= end && heap[child_id].rank < heap[child_id + 1].rank) {
+				++child_id;
+			}
+
+			if (heap[beg].rank < heap[child_id].rank) {
+				swap(&(heap[beg]), &(heap[child_id]));
+				beg = child_id;
+				child_id = 2 * beg + 1;
+			}
+			else {
+				return;
+			}
+		}
+
+	}
+	int32_t Minim_Heap::test(int index){
+		return (heap[index]).rank;
+	}
+	Minim_Heap::Minim_Heap(int size) {
+		heap.reserve(size); //the alternative could be reserving memory after determining the # of points in Rect, but it would cost us another O(N)
+	}
+	void Minim_Heap::clean() {
+		heap.clear();
+	}
+	int Minim_Heap::sizeof_heap() {
+		return heap.size();
+	}
+	bool Minim_Heap::isFull() {
+		if (heap.size() == capacity) {
+			return true;
+		}
+		else
+			return false;
+	}
+	void Minim_Heap::set_capacity() {
+
+	}
+	int check(Point* temp) {
+
+	}
+
+	__declspec(dllexport) int32_t  search(SearchContext* sc, const Rect rect, const int32_t count, Point* out_points)
+	{
+
+		/* 1st -  Search through the sc -> arr to determine all the points which are within the rect range in O(n) */
+		
+		//Minim_Heap* hp = new Minim_Heap(count); // create the heap size of "count" numbers
+		Point* temp_arr = (Point*)malloc(sizeof(Point)*count);
+		int ct = 1;
+		Point* it = sc->arr;
+		int index;
+		while (ct != (sc->count))
+		{
+			//std::cout << " ct:: " << ct << std::endl;
+			if (it->x >= rect.lx && it->x <= rect.hx && it->y >= rect.ly && it->y <= rect.hy) {
+				//hp -> insert(*it);
+				if (ct != count) {
+					temp_arr[ct - 1] = *it;
+				}
+				else {
+					index = check(it);
+					if (index != -1)
+						temp_arr[index] = *it;
+				}
+			}
+			++ct;
+		}
+		/*std::cout << "\nMIN POINT #0: " << hp->test(0) << std::endl;
+		std::cout << "MIN POINT #1: " << hp->test(1) << std::endl;
+		std::cout << "MIN POINT #2: " << hp->test(2) << std::endl;
+		std::cout << "MIN POINT #3: " << hp->test(3) << std::endl;
+		std::cout << "MIN POINT #4: " << hp->test(4) << std::endl;
+		std::cout << "MIN POINT #5: " << hp->test(5) << std::endl;
+		std::cout << "MIN POINT #6: " << hp->test(6) << std::endl;
+		std::cout << "MIN POINT #7: " << hp->test(7) << std::endl;*/
+
+		/*hp->create();
+		/* 3rd -  extract "count" numbers from the heap in O(N) 
+		Point* ex; // extracted point
+		int32_t j = 0; // counter for number of points extracted
+		for (int i = 1; i <= count; ++i) {
+			ex = hp->extract_min();
+			if (ex != NULL) {
+				out_points[j] = *ex;
+				++j;
+				delete ex;
+			}
+		}
+	*/
+		
+		return j;
+	}
+
+	__declspec(dllexport) SearchContext*  destroy(SearchContext* sc)
+	{
+		auto temp = sc->arr;
+		for(int i = 1; i <= sc->count; ++i){
+			free(temp);
+			++temp;
+		}
+	
+		if (sc->arr == NULL) {
+			delete sc;
+		}
+		if (sc == NULL) {
+			return nullptr;
+		}
+		else
+			return sc;
+	}
---
+++ BestRanks/point_search.h
@@ -0,0 +1,88 @@
+/* Given 10 million uniquely ranked points on a 2D plane, design a datastructure and an algorithm that can find the 20
+most important points inside any given rectangle. The solution has to be reasonably fast even in the worst case, while
+also not using an unreasonably large amount of memory.
+Create an x64 Windows DLL, that can be loaded by the test application. The DLL and the functions will be loaded
+dynamically. The DLL should export the following functions: "create", "search" and "destroy". The signatures and
+descriptions of these functions are given below. You can use any language or compiler, as long as the resulting DLL
+implements this interface. */
+#ifndef BESTRANKS__C
+#define BESTRANKS__C
+
+#include <stdint.h>
+
+	/* The following structs are packed with no padding. */
+#pragma pack(push, 1)
+
+/* Defines a point in 2D space with some additional attributes like id and rank. */
+	typedef struct
+	{
+		int8_t id;
+		int32_t rank;
+		float x;
+		float y;
+	}Point;
+
+	/* Defines a rectangle, where a point (x,y) is inside, if x is in [lx, hx] and y is in [ly, hy]. */
+	typedef struct
+	{
+		float lx;
+		float ly;
+		float hx;
+		float hy;
+	}Rect;
+#pragma pack(pop)
+
+	/* Declaration of the struct that is used as the context for the calls. */
+	typedef struct
+	{
+		//std::vector<Point> arr; //vector of Points 
+		Point* arr;
+		int count;
+	}SearchContext;
+
+	/* Declaration of the Minim_Heap class that is used for extraction of "count" points*/
+	class Minim_Heap
+	{
+	private:
+		std::vector <Point> heap;
+		int capacity;
+	public:
+		
+		Point* extract_min(); // get the lowest element in the heap
+		void min_heapify(int id, int end); // heapify down 
+		void insert(Point temp); //insert without heapifying
+		void create(); //create the heap
+		void swap(Point* X, Point* Y);
+		void clean(); // deallocates memory for heap vector
+		Minim_Heap(int size);
+		bool isFull(); // whether the heap full or not
+		void set_capacity(); // set the max size for the heap
+
+		// Helper functions for debugging
+		int32_t test(int index);
+		int sizeof_heap(); 
+
+
+	};
+
+
+	/* Load the provided points into an internal data structure. The pointers follow the STL iterator convention, where
+	"points_begin" points to the first element, and "points_end" points to one past the last element. The input points are
+	only guaranteed to be valid for the duration of the call. Return a pointer to the context that can be used for
+	consecutive searches on the data. */
+	typedef SearchContext* (__stdcall* T_create)(const Point * points_begin, const Point * points_end);
+
+	/* Search for "count" points with the smallest ranks inside "rect" and copy them ordered by smallest rank first in
+	"out_points". Return the number of points copied. "out_points" points to a buffer owned by the caller that
+	can hold "count" number of Points. */
+
+	typedef int32_t(__stdcall* T_search)(SearchContext* sc, const Rect rect, const int32_t count, Point* out_points);
+
+	/* Release the resources associated with the context. Return nullptr if successful, "sc" otherwise. */
+	typedef SearchContext* (__stdcall* T_destroy)(SearchContext* sc);
+
+	extern "C" __declspec(dllexport) SearchContext*  create(const Point* points_begin, const Point* points_end);
+	extern "C" __declspec(dllexport) int32_t  search(SearchContext* sc, const Rect rect, const int32_t count, Point* out_points);
+	extern "C" __declspec(dllexport) SearchContext*  destroy(SearchContext* sc);
+
+#endif
